974cc4f8cdfd65c5a357cbee1895f46c80a72c78
Joebester <88321326+Joebester@users.noreply.github.com>
2022-07-07 09:50:26 +0700

    jsdf

--- src/Pages/App/CRM/Col2/Package/Model/Tablepackage.jsx
+++ src/Pages/App/CRM/Col2/Package/Model/Tablepackage.jsx
@@ -1,132 +1,221 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import React from 'react'
+import MaterialTable from 'material-table'
+import { forwardRef } from 'react';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import { Dialog, DialogTitle, Grid, Table, TableBody, TableCell, TableHead, TableRow, Tooltip } from '@mui/material'
+import { AxiosReq } from '../../../../../../Components/Axios';
+import { Close, YoutubeSearchedFor } from '@mui/icons-material';
 
-const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
-];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <YoutubeSearchedFor {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
-
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-  container: {
-    maxHeight: 440,
-  },
-});
-
-export default function Tablepackage() {
-  const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-    console.log(newPage)
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
+export default function Tablepackage({ total }) {
+  const TextToDate = (text) => {
+    if (text.length > 0) {
+      let me = text.substr(0, 4) + "-" + text.substr(4, 2) + "-" + text.substr(6, 2) + " " + text.substr(8, 2) + ":" + text.substr(10, 2) + ":" + text.substr(12, 2)
+      return me
+    }
+  }
+  const [option, setOption] = React.useState({ bss: [], ocs: [], hlr: [], offering: [], pcrf: [], point: [] })
+  const [loadOp, setLoadOp] = React.useState({ load: false, id: '' })
+  const Columns = [
+    // { title: 'TransID', field: 'trans_id', maxWidth: 50, sorting: false, render: row => row.trans_id.length < 5 ? row.trans_id : <Tooltip title={row.trans_id}><div>...{row.trans_id?.substring(row.trans_id.length - 5, row.trans_id.length)}</div></Tooltip> },
+    { title: 'Msisdn', field: 'msisdn', minWidth: 150, sorting: false, render: row => row.msisdn.length <= 10 ? <div>{row.msisdn}</div> : <Tooltip title={row.msisdn}><div>{row.msisdn?.substring(row.msisdn.length - 10, row.msisdn.length)}</div></Tooltip> },
+    { title: 'StartDate', field: 'start_date', minWidth: 400, render: row => TextToDate(row.start_date) },
+    { title: 'StopDate', field: 'stop_date', minWidth: 400, render: row => TextToDate(row.stop_date) },
+    { title: 'UserID', field: 'user_id', minWidth: 100 },
+    // { title: 'Chanel', field: 'chanel', maxWidth: 100 },
+    // { title: 'DataCharging', field: 'data_charging', minWidth: 100 },
+    { title: 'PkCode', field: 'pkcode', minWidth: 100 },
+    { title: 'PkType', field: 'pktype', minWidth: 100 },
+    { title: 'SrvType', field: 'srvtype', minWidth: 200 },
+    { title: 'Amount', field: 'amount', minWidth: 200, type: 'numeric', render: row => row.amount?.toLocaleString() },
+    // { title: 'ResultCode', field: 'resultCode', maxWidth: 200 },
+    { title: 'ResultDesc', field: 'resultDesc', minWidth: 250, render: row => row.resultDesc.length <= 25 ? row.resultDesc : <Tooltip title={row.resultDesc}><div>{row.resultDesc?.substring(0, 25)}...</div></Tooltip> },
+    { title: 'ResultMessage', field: 'resultMessage', minWidth: 200, render: row => row.resultMessage.length <= 25 ? row.resultMessage : <Tooltip title={row.resultMessage}><div>{row.resultMessage?.substring(0, 25)}...</div></Tooltip> },
+  ]
+  const [data, setData] = React.useState({ data: [], page: 1, limit: 10 })
+  const [load, setLoad] = React.useState(false)
+  const [show, setShow] = React.useState(false)
+  const [tran, setTran] = React.useState('')
+  React.useEffect(() => {
+    let phone = localStorage.getItem("ONE_PHONE")
+    setLoad(true)
+    AxiosReq.get("New_PackageHistory?msisdn=" + phone + "&page=" + data.page + "&limit=" + data.limit + "&total=" + total).then(res => {
+      if (res.status === 200) {
+        setData({ ...data, data: res.data.data })
+        // console.log(res.data)
+        setLoad(false)
+      }
+    }).catch(er => {
+      setLoad(false)
+    })
+  }, [])
   return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+    <>
+      <MaterialTable
+        title={"Package History"}
+        icons={tableIcons}
+        columns={Columns}
+        data={data.data}
+        isLoading={load}
+        options={{
+          sorting: true,
+          exportButton: true,
+          pageSize: data.limit,
+          pageSizeOptions: [10, 20],
+          // columnsButton: true,
+          search: false
+        }}
+        actions={[
+          {
+            icon: tableIcons.Search,
+            tooltip: 'View More',
+            onClick: (event, rows) => {
+              let trans = rows.trans_id
+              console.log(trans)
+              let month = rows.month
+              setShow(true)
+              setTran(trans)
+              setLoadOp({ ...loadOp, load: true, id: trans })
+              AxiosReq.get("New_PackageLog?trans_id=" + trans + "&month=" + month).then(res => {
+                if (res.status === 200) {
+                  let resp = res.data
+                  setOption({
+                    ...option,
+                    bss: resp?.bss_log?.length > 0 ? resp.bss_log : [],
+                    ocs: resp?.ocs_log?.length > 0 ? resp.ocs_log : [],
+                    hlr: resp?.hlr_log?.length > 0 ? resp.hlr_log : [],
+                    offering: resp?.offering_log?.length > 0 ? resp.offering_log : [],
+                    pcrf: resp?.pcrf_log?.length > 0 ? resp.pcrf_log : [],
+                    point: resp?.point_log?.length > 0 ? resp.point_log : []
+                  })
+                  console.log(res.data)
+                  setLoadOp({ ...loadOp, load: false })
+                }
+              }).catch(er => {
+                setLoadOp({ ...loadOp, load: false })
+              })
+            }
+          }
+        ]}
       />
-    </Paper>
-  );
+
+      <Dialog
+        open={show}
+        onClose={() => setShow(false)}
+        maxWidth={1000}
+      >
+        <Grid container>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={6}>
+            <DialogTitle className='center'>TransID : {tran}</DialogTitle>
+          </Grid>
+          <Grid item xs={3}>
+            <div className='right'><Close className='icon' onClick={() => setShow(false)} /></div>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12} style={{ width: 1000 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>TransID</TableCell>
+                  <TableCell>Msisdn</TableCell>
+                  <TableCell>Chanel</TableCell>
+                  <TableCell>TransID</TableCell>
+                  <TableCell>TransID</TableCell>
+                  <TableCell>ResultCode</TableCell>
+                  <TableCell>ResultDesc</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell colSpan={7} className="head">OCS log</TableCell>
+                </TableRow>
+                {option?.ocs?.map((row, idx) => {
+                  return (
+                    <TableRow key={idx}>
+                      <TableCell>{row.trans_id}</TableCell>
+                      <TableCell>{row.msisdn.substr(row.msisdn.length - 10, row.msisdn.length)}</TableCell>
+                      <TableCell>{row.chanel}</TableCell>
+                      <TableCell>{row.main_product}</TableCell>
+                      <TableCell>{row.resultCode}</TableCell>
+                      <TableCell>{row.resultDesc}</TableCell>
+                    </TableRow>
+                  )
+                })}
+
+                <TableRow>
+                  <TableCell colSpan={7} className="head">PCRF log</TableCell>
+                </TableRow>
+                {option?.pcrf?.map((row, idx) => {
+                  return (
+                    <TableRow key={idx}>
+                      <TableCell>{row.trans_id}</TableCell>
+                      <TableCell>{row.msisdn.substr(row.msisdn.length - 10, row.msisdn.length)}</TableCell>
+                      <TableCell>{row.chanel}</TableCell>
+                      <TableCell>{row.cm_type}</TableCell>
+                      <TableCell>{row.detail}</TableCell>
+                      <TableCell>{row.resultCode}</TableCell>
+                      <TableCell>{row.resultDesc}</TableCell>
+                    </TableRow>
+                  )
+                })}
+
+                <TableRow>
+                  <TableCell colSpan={7} className="head">Point log</TableCell>
+                </TableRow>
+                {option?.point?.map((row, idx) => {
+                  return (
+                    <TableRow key={idx}>
+                      <TableCell>{row.trans_id}</TableCell>
+                      <TableCell>{row.msisdn.substr(row.msisdn.length - 10, row.msisdn.length)}</TableCell>
+                      <TableCell>{row.point}</TableCell>
+                      <TableCell>{row.resultCode}</TableCell>
+                      <TableCell>{row.resultDesc}</TableCell>
+                    </TableRow>
+                  )
+                })}
+
+              </TableBody>
+            </Table>
+          </Grid>
+        </Grid>
+      </Dialog>
+    </>
+  )
 }
