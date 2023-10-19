da08761f5b34780c888e3ba3caae64177205e9e6
soulixay <80397221+akhung123@users.noreply.github.com>
2023-10-19 14:58:05 +0700

    update function borrow data & money
    function borrow data & borrow money

--- src/Pages/App/CRM/Col2/Banking/Model/Borrow.jsx
+++ src/Pages/App/CRM/Col2/Banking/Model/Borrow.jsx
@@ -3,6 +3,7 @@ import MyTable from '../../../../../../Components/MyTable'
 import { AxiosReq } from '../../../../../../Components/Axios'
 import moment from 'moment'
 import { LoadingTable } from '../../../../../../Components/TableLoading'
+import cookie from 'js-cookie'
 
 function Borrow({ borData }) {
     const [stop, setStop] = React.useState(false)
@@ -10,26 +11,26 @@ function Borrow({ borData }) {
     React.useEffect(() => {
         setData(borData)
         setStop(true)
-        // var phone = localStorage.getItem("ONE_PHONE")
-        // AxiosReq.get("BorrowfadaoAndKalsym?msisdn_=" + phone).then(res => {
-        //     if (res.status === 200) {
-        //         var num = 0
-        //         var update = res.data.map(row => {
-        //             row.id_idx = num + 1
-        //             row.date_buy = moment(row.recordDate).format("DD-MM-YYYY HH:mm:ss")
-        //             row.date_pay = moment(row.lastdatetime).format("DD-MM-YYYY HH:mm:ss")
-        //             num = num + 1
-        //             return row
-        //         })
-        //         setData(update)
-        //         setTimeout(() => {
-        //             setStop(true)
-        //         }, 200)
-        //         // console.log(update)
-        //     }
-        // }).catch(err => {
-        //     setStop(true)
-        // })
+        var phone = localStorage.getItem("ONE_PHONE")
+        AxiosReq.get("BorrowfadaoAndKalsym?msisdn_=" + phone,{ headers: { 'Authorization': 'Bearer ' + cookie.get("ONE_TOKEN") } }).then(res => {
+            if (res.status === 200) {
+                var num = 0
+                var update = res.data.map(row => {
+                    row.id_idx = num + 1
+                    row.date_buy = moment(row.recordDate).format("DD-MM-YYYY HH:mm:ss")
+                    row.date_pay = moment(row.lastdatetime).format("DD-MM-YYYY HH:mm:ss")
+                    num = num + 1
+                    return row
+                })
+                setData(update)
+                setTimeout(() => {
+                    setStop(true)
+                }, 200)
+                // console.log(update)
+            }
+        }).catch(err => {
+            setStop(true)
+        })
     }, [])
     const columns = [
         { title: 'No', field: 'id_idx', maxWidth: 50 },
@@ -43,7 +44,7 @@ function Borrow({ borData }) {
         { title: 'ຄ່າທຳນຽມ', field: 'persentAmount', type: 'numeric', render: row => row.persentAmount > 0 ? row.persentAmount.toLocaleString() : row.persentAmount },
         { title: 'ທັງໝົດ', field: 'amount', type: 'numeric', render: row => row.amount > 0 ? row.amount.toLocaleString() : row.amount },
         { title: 'ເງິນທີ່ຈ່າຍ', field: 'paid', type: 'numeric', render: row => row.paid > 0 ? row.paid.toLocaleString() : <u className="red">{row.paid}</u> },
-        { title: 'ສະຖານະ', field: 'resultDesc' },
+        { title: 'ລາຍລະອຽດ', field: 'resultDesc' },
     ]
     function ShowData() {
         return (
