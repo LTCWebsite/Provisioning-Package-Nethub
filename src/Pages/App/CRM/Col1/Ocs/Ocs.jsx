eb59762e6837a3022a0754814b39897e8a85c033
larnaruto <larregister@gmail.com>
2024-03-15 14:14:56 +0700

    delete node modules

--- src/Pages/App/CRM/Col1/Ocs/Ocs.jsx
+++ src/Pages/App/CRM/Col1/Ocs/Ocs.jsx
@@ -21,7 +21,6 @@ function Ocs({ cus, load, st }) {
     const [pass, setPass] = useState(false)
     const [useIdel, setuseIdel] = useState(false)
 
-    // console.log(cus)
     useEffect(() => {
         setShow(load)
         let info = MyCryptTry("de", localStorage.getItem("ONE_DETAIL"))
